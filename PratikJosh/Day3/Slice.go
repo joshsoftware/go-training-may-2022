5b91adc8a8d2cfd78cbf2af8fd0d30d6fea8f30e
pratik <pratik.varute@joshsoftware.com>
2022-06-01 12:59:50 +0530

    Golang day wise 2

--- PratikJosh/Day3/Slice.go
+++ PratikJosh/Day3/Slice.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-
+	arr := [5]int{1, 2, 3, 4, 5}
 	s := []int{2, 3, 5, 7, 11, 13}
+
+	var a []int = arr[1:4]
+	fmt.Println(a)
+
 	printSlice(s)
 
 	s = s[:0]
