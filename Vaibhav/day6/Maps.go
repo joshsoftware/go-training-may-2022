ac12ef82c347e5f7802a88442af795141def53e1
vaibhavgholap23 <vaibhavgholap23@gmail.com>
2022-06-03 11:05:28 +0530

    maps 2 added

--- Vaibhav/day6/Maps.go
+++ Vaibhav/day6/Maps.go
@@ -6,15 +6,31 @@ func main() {
 
 	// Creating a map
 	// map[key-type]value-type
-	var Yashwin = make(map[int]string)
-	fmt.Println(Yashwin)
+	// var Yashwin = make(map[int]string)
+	// fmt.Println(Yashwin)
 
-	// Adding elements to the map
-	Yashwin[1] = "Rohit"
-	Yashwin[2] = "Sumit"
-	Yashwin[3] = "Vaibhav"
-	Yashwin[4] = "Rajat"
+	// // Adding elements to the map
+	// Yashwin[1] = "Rohit"
+	// Yashwin[2] = "Sumit"
+	// Yashwin[3] = "Vaibhav"
+	// Yashwin[4] = "Rajat"
 
-	fmt.Println(Yashwin)
+	// fmt.Println(Yashwin)
+
+	countries := map[string]string{
+		"IN": "India",
+		"NP": "Nepal",
+		"TR": "Turkey",
+		"JP": "Japan",
+		"ZW": "Zimbabwe",
+	}
+
+	for country := range countries {
+		fmt.Println(country, "=>", countries[country])
+	}
+
+	for key, value := range countries {
+		fmt.Printf("countries[%s] = %s\n", key, value)
+	}
 
 }
